2726b9b0182edb7aaf8e9ee4580fb197bb9617af
Evgeniy Savenko <wayrez27@gmail.com>
2022-12-03 00:21:13 +0300

    added form for order

---
+++ src/components/DialogForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dialog" v-if="show === true">
+    <div class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PceDialogForm',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dialog {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+.dialog__content {
+  margin: auto;
+  background: white;
+  border-radius: 20px;
+  min-width: 350px;
+  min-height: 100px;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Consultation from '@/components/Consultation';
 import Promo from '@/components/Promo';
 import Footer from '@/components/Footer';
 import Price from '@/components/Price';
+import DialogForm from '@/components/DialogForm';
 
-export default [TopBar, Button, Consultation, Promo, Footer, Price];
+export default [TopBar, Button, Consultation, Promo, Footer, Price, DialogForm];
