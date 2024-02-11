7a351d38d68abce7f95b5737dac3d6cec9a4d42c
Denis Morozov <morden28@yandex.ru>
2024-02-12 01:41:55 +0300

    notification mock tests

--- notification/internal/postgres/postgres.go
+++ notification/internal/postgres/postgres.go
@@ -1,15 +1,27 @@
-package database
+package postgres
 
 import (
 	"context"
 	"database/sql"
 	"fmt"
 
+	"github.com/Impisigmatus/PestControlExpert/notification/autogen"
+	"github.com/Impisigmatus/PestControlExpert/notification/internal/models"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
 )
+
+//go:generate mockgen -source=postgres.go -package mocks -destination ../../autogen/mocks/postgres.go
+type IDatabase interface {
+	GetSubscribers() ([]models.Subscriber, error)
+	AddSubscriber(subscriber models.Subscriber) (bool, error)
+	PushNotification(tx *sqlx.Tx, notification autogen.Notification) error
+	GetTX() (*sqlx.Tx, error)
+	CommitTX(tx *sqlx.Tx) error
+	RollbackTX(tx *sqlx.Tx) error
+}
 
 type PostgresConfig struct {
 	Hostname string
@@ -25,7 +37,7 @@ type Postgres struct {
 
 const driver = "pgx"
 
-func NewPostgres(cfg PostgresConfig) Database {
+func NewPostgres(cfg PostgresConfig) IDatabase {
 	return &Postgres{db: sqlx.NewDb(newPostgres(cfg), driver)}
 }
 
@@ -56,4 +68,12 @@ func newPostgres(cfg PostgresConfig) *sql.DB {
 
 func (pg *Postgres) GetTX() (*sqlx.Tx, error) {
 	return pg.db.BeginTxx(context.Background(), nil)
+}
+
+func (pg *Postgres) CommitTX(tx *sqlx.Tx) error {
+	return tx.Commit()
+}
+
+func (pg *Postgres) RollbackTX(tx *sqlx.Tx) error {
+	return tx.Rollback()
 }
---
+++ notification/internal/telegram/bot_test.go
@@ -0,0 +1,94 @@
+package telegram
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/Impisigmatus/PestControlExpert/notification/autogen"
+	"github.com/Impisigmatus/PestControlExpert/notification/autogen/mocks"
+	"github.com/Impisigmatus/PestControlExpert/notification/internal/models"
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/golang/mock/gomock"
+	"github.com/jmoiron/sqlx"
+)
+
+func Test_TelegramBot(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	db := mocks.NewMockIDatabase(ctrl)
+	api := mocks.NewMockITelegramAPI(ctrl)
+	bot := newBot(db, api, "test")
+
+	t.Run("Notify()", func(t *testing.T) {
+		sample_tx := &sqlx.Tx{Tx: &sql.Tx{}}
+		t.Run("With subscribers", func(t *testing.T) {
+			sample_notification := autogen.Notification{
+				Phone:       "+79991234567",
+				Name:        "Василий",
+				Description: nil,
+			}
+			db.EXPECT().GetTX().Return(sample_tx, nil)
+			db.EXPECT().PushNotification(sample_tx, sample_notification).Return(nil)
+			db.EXPECT().CommitTX(sample_tx).Return(nil)
+			db.EXPECT().GetSubscribers().Return([]models.Subscriber{{}}, nil)
+			api.EXPECT().Send(gomock.Any()).Return(tg.Message{}, nil)
+
+			if err := bot.Notify(sample_notification); err != nil {
+				t.Fatalf("Invalid PostApiNotify(): %s", err)
+			}
+		})
+
+		t.Run("Without subscribers", func(t *testing.T) {
+			sample_notification := autogen.Notification{
+				Phone:       "+79991234567",
+				Name:        "Василий",
+				Description: nil,
+			}
+
+			db.EXPECT().GetTX().Return(sample_tx, nil)
+			db.EXPECT().PushNotification(sample_tx, sample_notification).Return(nil)
+			db.EXPECT().CommitTX(sample_tx).Return(nil)
+			db.EXPECT().GetSubscribers().Return([]models.Subscriber{}, nil)
+
+			if err := bot.Notify(sample_notification); err != nil {
+				t.Fatalf("Invalid Notify(): %s", err)
+			}
+		})
+	})
+
+	t.Run("handle()", func(t *testing.T) {
+		update := tg.Update{Message: &tg.Message{
+			Text: "test",
+			Chat: &tg.Chat{ID: 0},
+			From: &tg.User{
+				UserName:  "test_username",
+				FirstName: "test_name",
+			},
+		}}
+		t.Run("New subscriber", func(t *testing.T) {
+			db.EXPECT().AddSubscriber(models.Subscriber{
+				ChatID:   update.Message.Chat.ID,
+				Username: update.Message.From.UserName,
+				Name:     update.Message.From.FirstName,
+			}).Return(false, nil)
+			api.EXPECT().Send(tg.NewMessage(update.Message.Chat.ID, "Вы успешно подписаны на оповещения от сервисов сайта Pest Control Expert")).Return(tg.Message{}, nil)
+
+			bot.handle(update)
+		})
+		t.Run("Exists subscriber", func(t *testing.T) {
+			db.EXPECT().AddSubscriber(models.Subscriber{
+				ChatID:   update.Message.Chat.ID,
+				Username: update.Message.From.UserName,
+				Name:     update.Message.From.FirstName,
+			}).Return(true, nil)
+			api.EXPECT().Send(tg.NewMessage(update.Message.Chat.ID, "Вы уже были подписаны на оповещения от сервисов сайта Pest Control Expert")).Return(tg.Message{}, nil)
+
+			bot.handle(update)
+		})
+
+		t.Run("Empty update", func(t *testing.T) {
+			bot.handle(tg.Update{})
+		})
+	})
+}
