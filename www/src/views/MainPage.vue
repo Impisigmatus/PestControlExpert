7959b35238235c5d4d7b788e8035bb62743c07b5
Денис Морозов <31116947+Impisigmatus@users.noreply.github.com>
2024-02-11 20:20:35 +0300

    Update MainPage.vue

--- www/src/views/MainPage.vue
+++ www/src/views/MainPage.vue
@@ -137,7 +137,7 @@
               />
               <span>
                 Я согласен(а) с
-                <a target="/privacy" href="/privacy">
+                <a href="/privacy.html">
                   политикой конфидициальности
                 </a>
               </span>
