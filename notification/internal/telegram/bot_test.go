dcba7f5e458d0b6c934db5dbfcdfb1e13b8e9ce5
Denis Morozov <morden28@yandex.ru>
2024-02-12 01:51:44 +0300

    ref

--- notification/internal/telegram/bot_test.go
+++ notification/internal/telegram/bot_test.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
 
 	"github.com/Impisigmatus/PestControlExpert/notification/autogen"
@@ -22,12 +23,12 @@ func Test_TelegramBot(t *testing.T) {
 
 	t.Run("Notify()", func(t *testing.T) {
 		sample_tx := &sqlx.Tx{Tx: &sql.Tx{}}
+		sample_notification := autogen.Notification{
+			Phone:       "+79991234567",
+			Name:        "Василий",
+			Description: nil,
+		}
 		t.Run("With subscribers", func(t *testing.T) {
-			sample_notification := autogen.Notification{
-				Phone:       "+79991234567",
-				Name:        "Василий",
-				Description: nil,
-			}
 			db.EXPECT().GetTX().Return(sample_tx, nil)
 			db.EXPECT().PushNotification(sample_tx, sample_notification).Return(nil)
 			db.EXPECT().CommitTX(sample_tx).Return(nil)
@@ -38,14 +39,7 @@ func Test_TelegramBot(t *testing.T) {
 				t.Fatalf("Invalid PostApiNotify(): %s", err)
 			}
 		})
-
 		t.Run("Without subscribers", func(t *testing.T) {
-			sample_notification := autogen.Notification{
-				Phone:       "+79991234567",
-				Name:        "Василий",
-				Description: nil,
-			}
-
 			db.EXPECT().GetTX().Return(sample_tx, nil)
 			db.EXPECT().PushNotification(sample_tx, sample_notification).Return(nil)
 			db.EXPECT().CommitTX(sample_tx).Return(nil)
@@ -53,6 +47,15 @@ func Test_TelegramBot(t *testing.T) {
 
 			if err := bot.Notify(sample_notification); err != nil {
 				t.Fatalf("Invalid Notify(): %s", err)
+			}
+		})
+		t.Run("Invalid push", func(t *testing.T) {
+			db.EXPECT().GetTX().Return(sample_tx, nil)
+			db.EXPECT().PushNotification(sample_tx, sample_notification).Return(fmt.Errorf("some error"))
+			db.EXPECT().RollbackTX(sample_tx).Return(nil)
+
+			if err := bot.Notify(sample_notification); err == nil {
+				t.Fatalf("Invalid Notify(): error does not exists")
 			}
 		})
 	})
@@ -66,14 +69,14 @@ func Test_TelegramBot(t *testing.T) {
 				FirstName: "test_name",
 			},
 		}}
+		subscriber := models.Subscriber{
+			ChatID:   update.Message.Chat.ID,
+			Username: update.Message.From.UserName,
+			Name:     update.Message.From.FirstName,
+		}
 		t.Run("New subscriber", func(t *testing.T) {
-			db.EXPECT().AddSubscriber(models.Subscriber{
-				ChatID:   update.Message.Chat.ID,
-				Username: update.Message.From.UserName,
-				Name:     update.Message.From.FirstName,
-			}).Return(false, nil)
+			db.EXPECT().AddSubscriber(subscriber).Return(false, nil)
 			api.EXPECT().Send(tg.NewMessage(update.Message.Chat.ID, "Вы успешно подписаны на оповещения от сервисов сайта Pest Control Expert")).Return(tg.Message{}, nil)
-
 			bot.handle(update)
 		})
 		t.Run("Exists subscriber", func(t *testing.T) {
@@ -83,10 +86,8 @@ func Test_TelegramBot(t *testing.T) {
 				Name:     update.Message.From.FirstName,
 			}).Return(true, nil)
 			api.EXPECT().Send(tg.NewMessage(update.Message.Chat.ID, "Вы уже были подписаны на оповещения от сервисов сайта Pest Control Expert")).Return(tg.Message{}, nil)
-
 			bot.handle(update)
 		})
-
 		t.Run("Empty update", func(t *testing.T) {
 			bot.handle(tg.Update{})
 		})
